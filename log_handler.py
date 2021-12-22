a366c50ad270d2cb29e9eaade0230044ab4bfabe
Pedro Madrigal <pmadrigal@ebi.ac.uk>
2021-12-22 18:21:14 +0000

    Add logging to tracking events (mostly errors and chunk ids). Upon termination of the workflow it finds and prints files with missing and failed accessions for the species that is being indexed

--- log_handler.py
+++ log_handler.py
@@ -1,49 +1,74 @@
 import sys
 import logging
 import os
+import re
 
 l = logging.getLogger(__name__)
 l.setLevel(logging.DEBUG)
 fh = logging.FileHandler(os.environ.get('LOG_PATH'), mode='w')
 l.addHandler(fh)
 
-jobs = {}
+#jobs = {}
+error_list=[]
+job_error_list=[]
+msg_error_list=[]
+chunks=[]
+# logging of errors in the workflow
 
 def log_handler(msg):
+    #for k, v in msg.items():
+    #    l.info(f"Key: {k}")
+    #    l.info(f"Value: {v}")
 
-    if "jobid" in msg:
-        if msg['jobid'] in jobs:
-            if 'level' in msg:
-                jobs[msg['jobid']]['status'] = msg['level']
-        else:
-            job = { 'name': msg['name'], 'status': 'created' }
-            if 'accession' in msg['wildcards']:
-                job['accession'] = msg['wildcards']['accession']
-            if 'log' in msg and len(msg['log']) > 0:
-                job['log'] = msg['log'][0]
-            jobs[msg['jobid']] = job
+    #l.info(f"new msg")
+    if "level" in msg:
+        if msg['level'] == 'error':
+            l.info(f"---error found")
+            error_list.append(msg['level'])
+        if msg['level'] == 'job_error':
+            l.info(f"---job_error found")
+            job_error_list.append(msg['level'])
 
-        #for k, v in msg.items():
-        #    print(f"Key: {k}")
-        #    print(f"Value: {v}")
+    if 'wildcards' in msg:
+        if msg['wildcards']:
+            chunks.append(msg['wildcards']['chunk'])
+            l.info(f"---Wildcards found in msg:\t{chunks}")
 
-        jobs_to_remove = []
-        for j_id, j_cont in jobs.items():
-            if 'accession' in j_cont and j_cont['status'] != 'created':
-                log=""
-                if 'log' in j_cont:
-                    log=j_cont['log']
-                l.info(f"{j_cont['accession']}\t{j_cont['name']}\t{j_cont['status']}\t{log}")
-                jobs_to_remove.append(j_id)
-                #sys.stdout.flush()
 
-        for j_id in jobs_to_remove:
-            del jobs[j_id]
+    if 'msg' in msg:
+        A=msg['msg']
+        if A != None and isinstance(A, str):
+            if re.search("Error executing rule", A, re.IGNORECASE) :
+                l.info(f"---\t{A}")
+                msg_error_list.append(A)
+            # end of the workflow:
+            elif re.search("removed all locks", A, re.IGNORECASE) :
 
-    #if msg.get('level') == 'progress':
-    #    cur = msg['done']
-    #    total = msg['total']
-    #    print(get_bar(cur, total) + f" {cur}/{total}")
-    #if 'msg' in msg and msg['msg'] == 'removed all locks':
-    #    sys.stdout.close()
-    
+                l.info(f"---------------------")
+                l.info(f"Log handler- Summary:")
+                l.info(f"{error_list}" )
+                l.info(f"{job_error_list}" )
+                l.info(f"{msg_error_list}" )
+
+                l.info(f"length of error_list = {len(error_list)}" )
+                l.info(f"length of job_error_list = {len(job_error_list)}" )
+                l.info(f"length of msg_error_list = {len(msg_error_list)}" )
+
+                l.info(f"Some errors shown above may have been solved through retrials")
+                l.info(f"---------------------")
+                l.info(f"Detect and print content of files with failed or missing accessions:")
+                wdir = os.getcwd()
+                folders=["analytics_jsonl_files", "load_bulk_analytics_index", "update_coexpressions", "update_experiment_designs"]
+                filetype=["missing", "failed"]
+                if set(chunks):
+                    for folder in folders:
+                        for ch in set(chunks):
+                            for fltp in filetype:
+                                acc_info_file=wdir+"/"+folder+"/"+ch+"/"+fltp+"_accessions.txt"
+                                if os.path.isfile(acc_info_file):
+                                    l.info(f"File found:  \t{acc_info_file}")
+                                    f = open(acc_info_file, 'r')
+                                    file_contents = f.read()
+                                    l.info(f"\t{file_contents}")
+                                    f.close()
+
