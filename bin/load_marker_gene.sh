51569ad63a9baf823230eb5bfbf4bb00ca0aade7
Anil Thanki <thanki.anil@gmail.com>
2025-07-03 14:16:05 +0100

    Update load_marker_gene.sh - updates checkDatabaseConnection

--- bin/load_marker_gene.sh
+++ bin/load_marker_gene.sh
@@ -16,16 +16,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}" )" &> /dev/null && pw
 postgres_scripts_dir="${SCRIPT_DIR}/../postgres_routines"
 
 checkDatabaseConnection() {
-  pg_user=$(echo $1 | sed s+postgresql://++ | awk -F':' '{ print $1}')
-  pg_host_port=$(echo $1 | awk -F':' '{ print $3}' \
-           | awk -F'@' '{ print $2}' | awk -F'/' '{ print $1 }')
-  pg_host=$(echo $pg_host_port  | awk -F':' '{print $1}')
-  pg_port=$(echo $pg_host_port  | awk -F':' '{print $2}')
-  if [ ! -z "$pg_port" ]; then
-    pg_isready -U $pg_user -h $pg_host -p $pg_port || (echo "No db connection." && exit 1)
-  else
-    pg_isready -U $pg_user -h $pg_host || (echo "No db connection" && exit 1)
-  fi
+  psql $1 -c '\q' > /dev/null 2>&1 || { echo "PostgreSQL is not ready"; }
 }
 
 dbConnection=${dbConnection:-$1}
