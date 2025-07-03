ded1d54e7b735c448f1d97d9c2a559201662bbfa
Anil Thanki <thanki.anil@gmail.com>
2025-07-03 11:52:55 +0100

    Update load_marker_gene.sh - accommodate fpkm and tpm from separate file

--- bin/load_marker_gene.sh
+++ bin/load_marker_gene.sh
@@ -17,25 +17,35 @@ postgres_scripts_dir="${SCRIPT_DIR}/../postgres_routines"
 
 dbConnection=${dbConnection:-$1}
 EXP_ID=${EXP_ID:-$2}
+metrices="tpm fpkm" # add proteomics
 
 # Check that necessary environment variables are defined.
 [ -z ${dbConnection+x} ] && echo "Env var dbConnection for the database connection needs to be defined. This includes the database name." && exit 1
 [ -z ${EXP_ID+x} ] && echo "Env var EXP_ID for the id/accession of the experiment needs to be defined." && exit 1
 
 # Check that files are in place.
-marker_genes_path=$ATLAS_PROD/analysis/baseline/*/experiments/${EXP_ID}/${EXP_ID}_marker_gene.tsv
-
-[ -e "$marker_genes_path" ] || { echo "$EXP_ID: marker_gene.tsv missing, exiting."; exit 1; }
-
+for metric in $metrices;
+do
+    marker_genes_path=$ATLAS_PROD/analysis/baseline/*/experiments/${EXP_ID}/${EXP_ID}-${metric}-markers.tsv
+    
+    [ -e "$marker_genes_path" ] || { echo "For $EXP_ID: ${EXP_ID}-${metric}-markers.tsv missing, exiting."; exit 1; }
+    
+done
 
 # Check that database connection is valid
 checkDatabaseConnection $dbConnection
-
+    
 # Deletes existing marker genes from gxa_marker_gene table 
 sed "s/<EXP-ACCESSION>/$EXP_ID/" $postgres_scripts_dir/01-delete_existing_marker_gene.sql.template | \
 psql -v ON_ERROR_STOP=1 "$dbConnection"
 
-# Load partition table in the same transaction.
-sed "s/<EXP-ACCESSION>/$EXP_ID/" $postgres_scripts_dir/02-load_gene_marker_table.sql.template | \
-    sed "s+<PATH-TO-DATA>+$marker_genes_path+" | \
-    psql -v ON_ERROR_STOP=1 $dbConnection
+# Load gene marker table
+    
+for metric in $metrices;
+do
+    marker_genes_path=$ATLAS_PROD/analysis/baseline/*/experiments/${EXP_ID}/${EXP_ID}-${metric}-markers.tsv
+    
+    sed "s/<EXP-ACCESSION>/$EXP_ID/" $postgres_scripts_dir/02-load_gene_marker_table.sql.template | \
+        sed "s+<PATH-TO-DATA>+$marker_genes_path+" | \
+        psql -v ON_ERROR_STOP=1 $dbConnection
+done
