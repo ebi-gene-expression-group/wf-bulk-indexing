a4af2d2a744461d0aa9ecfe4b81ea64b82da4907
Anil Thanki <thanki.anil@gmail.com>
2025-07-04 15:10:22 +0100

    Update load_marker_gene.sh - adds `error_exit`

--- bin/load_marker_gene.sh
+++ bin/load_marker_gene.sh
@@ -11,6 +11,12 @@
 # 
 set -e
 
+error_exit() {
+  echo "[ERROR] $1" >&2
+  exit 1
+}
+
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}" )" &> /dev/null && pwd )
 
 postgres_scripts_dir="${SCRIPT_DIR}/../postgres_routines"
