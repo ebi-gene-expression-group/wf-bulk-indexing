1c9dcb395c6db19f7f7863b399fc145b54166927
Anil Thanki <thanki.anil@gmail.com>
2025-11-28 13:11:44 +0000

    Create scripts to load marker genes into postgres (#9)
    * Create load_marker_gene.sh
    
    * Create 01-delete_existing_marker_gene.sql.template
    
    * Create 02-load_gene_marker_table.sql.template
    
    ---------
    
    Co-authored-by: Pedro Madrigal <8195212+pmb59@users.noreply.github.com>

---
+++ bin/load_marker_gene.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+# This script takes the marker_gene files (normally available on
+# staging) for a gxa experiment and does the following steps:
+# - Transforms those files into a long (melted) table of experiment id,
+#   cell/run id, and expression. This takes care of avoiding large chunks of
+#   data being kept in memory for long, at the expense of writing to disk (too)
+#   often.
+# - Deletes data from gxa_marker_gene table on postgres if exists 
+# - Loads data into the gxa_marker_gene table.
+# 
+set -e
+
+error_exit() {
+  echo "[ERROR] $1" >&2
+  exit 1
+}
+
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd)"
+POSTGRES_SCRIPTS_DIR="${SCRIPT_DIR}/../postgres_routines"
+METRICS=("tpms" "fpkms")  # Add more metrics as needed
+
+EXP_ID=${EXP_ID:-$1}
+
+# Check that necessary environment variables are defined.
+[ -z ${dbConnection+x} ] && error_exit  "Env var dbConnection for the database connection needs to be defined."
+[ -z ${EXP_ID+x} ] && error_exit  "Env var EXP_ID for the id/accession of the experiment needs to be defined."
+
+
+# Check that database connection is valid
+check_db_connection() {
+  psql "$dbConnection" -c '\q' &>/dev/null || error_exit "PostgreSQL is not ready or connection failed."
+  echo "Database connection established using $dbConnection."
+}
+    
+# Deletes existing marker genes from gxa_marker_gene table 
+delete_old_data() {
+  local sql_file="${POSTGRES_SCRIPTS_DIR}/01-delete_existing_marker_gene.sql.template"
+  [[ ! -f "$sql_file" ]] && error_exit "SQL template not found: $sql_file"
+  sed "s/<EXP-ACCESSION>/$EXP_ID/" "$sql_file" | psql -v ON_ERROR_STOP=1 "$dbConnection"
+}
+
+find_marker_file() {
+  echo "find_marker_file" >&2
+  local metric="$1"
+  echo "metric@${metric}@" >&2
+  local path_pattern="${ATLAS_EXPS}/${EXP_ID}/${EXP_ID}-${metric}markers.tsv"
+  echo "path_pattern@${path_pattern}@" >&2
+  local file
+  file=$(ls $path_pattern 2>/dev/null | head -n1)
+  echo "${file}"
+}
+
+# Load gene marker table
+load_marker_data() {
+  local metric="$1"
+  local marker_file
+  marker_file=$(find_marker_file "$metric")
+  
+  if [[ -z "$marker_file" ]]; then
+    echo "${1} Marker file not found."
+  else
+    echo "Processing file: $marker_file"
+  
+    local no_header_file="${marker_file}.no_header.tsv"
+    tail -n +2 "$marker_file" > "$no_header_file"
+  
+    local sql_file="${POSTGRES_SCRIPTS_DIR}/02-load_gene_marker_table.sql.template"
+    [[ ! -f "$sql_file" ]] && error_exit "SQL template not found: $sql_file"
+    sed "s|<PATH-TO-DATA>|$no_header_file|" "$sql_file" | psql -v ON_ERROR_STOP=1 "$dbConnection"
+    rm -f "$no_header_file"
+  fi
+}
+
+
+check_db_connection
+delete_old_data
+
+found_any=0
+
+if [[ $EXP_ID =~ ^E-PROT-[0-9]+$ ]]; then
+      load_marker_data ""
+      found_any=1
+else
+  for metric in "${METRICS[@]}";
+  do
+      marker_file=$(find_marker_file "$metric-")
+      if [[ -n "$marker_file" ]]; then
+        load_marker_data "$metric-"
+        found_any=1
+      else
+        echo "WARNING: No marker file found for metric: ${metric} in ${EXP_ID}"
+      fi
+  done
+fi
+
+if [[ $found_any -eq 0 ]]; then
+    error_exit "ERROR: No marker files found for ${EXP_ID}."
+fi
